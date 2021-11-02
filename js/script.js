c311afd9bebd5339326c55879361cfd601d4f0cd
AbefullStackDeveloper <84894159+AbefullStackDeveloper@users.noreply.github.com>
2021-11-02 17:01:50 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log("test");
+//const MyName="Abby";
+const MyChild = "Abel,Eva";
+console.log(MyChild);
+    
+ const people = [
+     {
+        name: "Abby",
+       isInstructor: true,
+        score: 90,
+        hairColor: 'Brown',
+         favoriteFoods: [],
+         friends: []
+   }
+
+];
+
+console.log(people);
+
+// Change heading:
+document.getElementById("myH").innerHTML = "My First Page";
+
+// Change paragraph:
+document.getElementById("myP").innerHTML = "My first paragraph."
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+*/
+document.getElementById("myH").innerHTML = "My First Page";
+document.getElementById("myP").innerHTML = "My first paragraph.";
